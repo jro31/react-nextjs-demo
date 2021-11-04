90127b987d56e5e9045bb9e75af743aebd2b24ab
Jethro Williams <jethrowilliams@outlook.com>
2021-11-04 10:48:26 +0000

    Add two components in the 'pages' folder
    Somehow both are already reachable at 'my-domain.com' and 'my-domain.com/news'

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+// 'index.js' is the root file
+// So if the url is 'my-domain.com' (with nothing afterwards) then 'index.js' will be loaded
+
+const HomePage = () => {
+  return <h1>The Home Page</h1>;
+};
+
+export default HomePage;
---
+++ pages/news.js
@@ -0,0 +1,7 @@
+// The news.js file will be served if the URL is 'my-domain.com/news'
+
+const NewsPage = () => {
+  return <h1>The News Page</h1>;
+};
+
+export default NewsPage;
